08d6d64d98d28fa1c5161790ebebff5c4ac23728
Braden O'Guinn <braden.oguinn@mutualmobile.com>
2016-10-26 10:09:02 -0500

    Adds hapi-harvester and polyglot
    Signed-off-by: Braden O'Guinn <braden.oguinn@mutualmobile.com>

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const config = require('config');
 const Hapi = require('hapi');
+const harvester = require('hapi-harvester');
 const injectThen = require('inject-then');
+const polyglot = require('hapi-harvester-polyglot');
 const port = config.get('server.port');
 const server = new Hapi.Server();
 
+console.log('config', config.db);
+
+const harvesterRegister = {
+  register: harvester,
+  options: { adapter: harvester.getAdapter('mongodb')(config.db.mongodbUrl) }
+};
+
 server.connection({ port });
 module.exports = () =>
-  server.register(injectThen)
+  server.register([injectThen,
+    harvesterRegister,
+    polyglot])
   .then(() => {
     console.log('Hapi app listening on: ', port);
     server.route({
--- config/default.js
+++ config/default.js
@@ -1,5 +1,9 @@
 module.exports = {
   server: {
     port: process.env.PORT || 9000
+  },
+  db: {
+    mongodbUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/test',
+    oplogConnectionString: process.env.MONGODB_OPLOG_URL || 'mongodb://localhost:27017/local'
   }
 };
